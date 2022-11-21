855a614f214dd72db4caeceac5b433cedd562a58
wayne <wayne1024.cn@outlook.com>
2022-11-21 21:04:22 +0800

    修改测试用例。

--- Program.cs
+++ Program.cs
@@ -11,6 +11,12 @@ namespace MqttExample
     {
         static async Task Main(string[] args)
         {
+            // 简单测试mqtt
+            await Test01();
+        }
+
+        public static async Task Test01()
+        {
             MqttClient mqttClient = new MqttClient();
             await mqttClient.ConnectAsync();
             await mqttClient.PublishPayload("api/v1/test", "Hello World！");
